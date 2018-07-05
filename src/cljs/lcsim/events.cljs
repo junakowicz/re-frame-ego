5f057718be901bfd00f15ac8e57cac211e0e0263
andrzej.wower@youview.com <andrzej.wower@youview.com>
2018-07-05 10:36:35 +0100

    Adds letter shapes

--- src/cljs/lcsim/events.cljs
+++ src/cljs/lcsim/events.cljs
@@ -10,24 +10,30 @@
  (fn [_ _]
    db/default-db))
 
+
+(defn offset-cells [cells-pos offset-xy]
+   (let [cells-x (for [pos cells-pos] (first pos))
+         cells-y (for [pos cells-pos] (second pos))
+         dx (:x offset-xy)
+         dy (:y offset-xy)
+         tox (map #(+ dx %) cells-x)
+         toy (map #(+ dy %) cells-y)]
+      [tox toy]
+     ))
+
+
 (re-frame/reg-event-db
  ::move
  (fn [db [e _ _]]
    (let [cells-pos (:marked-cells db)
-         cells-x (for [pos cells-pos] (first pos))
-         cells-y (for [pos cells-pos] (second pos))
-         dimensions (:grid-dimensions db)
          direction (:direction db)
-         dx (:x direction)
-         dy (:y direction)
-         tox (map #( + dx %) cells-x)
-         toy (map #(+ dy %) cells-y)
+         [tox toy] (offset-cells cells-pos direction)
+         dimensions (:grid-dimensions db)
          newpositions (utils/wrap-move dimensions tox toy)
-         vex (first newpositions)
-         vey (second newpositions)
-         veo (apply map vector [vex vey])
+        ;  [vex vey] newpositions
+         veo (apply map vector newpositions);recreate to format [[1 1] [0 1]]
          ]
-     (println "============" e  cells-x cells-y "tox" tox "newpositions" newpositions "vex" vex "veo" veo)
+    ;  (println "============" e "tox" tox "newpositions" newpositions "vex" vex "veo" veo)
      (assoc db :marked-cells veo))))
 
 (re-frame/reg-event-db
